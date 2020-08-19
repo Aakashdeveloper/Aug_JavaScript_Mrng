4edbd7d5739b7c9e28fcfa4993fdfd09ad4c250e
ahand14 <aakash.handa@aexp.com>
2020-08-19 08:40:47 +0530

    A

---
+++ first.js
@@ -0,0 +1,116 @@
+version
+
+>Es> Ecma Script 
+JavaScript
+TypeScript
+
+Es5 > old
+Es6 > new
+
+//////////////////
+dataType
+>String = "dvf" 'dsgf' "454" '554' 'true' "false"
+>Number = 47, 857685686, 44.5555 , 4
+>Boolean = true / false   
+>null
+>undefined
+
+//Es5////
+var a = "hi"
+var b = 10
+var c = true
+
+a
+"hi"
+b
+10
+c
+true
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = "10"
+typeof(d)
+"string"
+var e = 10.998
+typeof(e)
+"number"
+
+var a = 10
+var b = 5
+a+b
+15
+a-b
+5
+a*b
+50
+a/b
+2
+a%b
+10%5
+0
+4%3
+1
+4%2
+0
+
+2%4
+3%4
+4%2
+0
+2%4
+2
+3%4
+3
+
+modules of smaller to larger its always smaller
+
+var a = "Hi "
+var b = "JavaScript"
+a+b
+"Hi JavaScript"
+
+if we use + with number they will add
+if we use + with string they will concat
+
+var a = "Hi "
+var b = "JavaScript"
+undefined
+a+b
+"Hi JavaScript"
+var c = 10
+undefined
+a+b+c
+"Hi JavaScript10"
+c+a
+"10Hi "
+a-b
+NaN > Not a Number
+a*b
+NaN
+a/b
+NaN
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+"10"+10+"10"
+"1010"
+"101010"
+10+10+"10"
+"2010"
+"10"+10+10
+"1010"+10
+"101010"
+
+"10"+10+"10"-1
+101009
+10+10+"10"-1
+2009
+"10"+10+10-1
+101009
